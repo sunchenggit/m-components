10d3fea6a3b514684b05377c59779f2fd732be41
suncheng <157600408@qq.com>
2022-04-21 09:49:36 +0800

    完成城市选择

--- src/views/citySelect/index.vue
+++ src/views/citySelect/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <div><m-city-select /></div>
+  <div>
+    <m-city-select
+      @changeCity="hanldeChooseCity"
+      @changeProvice="hanldeChooseProvice"
+    />
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { City } from "../../components/citySelect/src/types";
+let hanldeChooseCity = (item: City) => {
+  console.log(item);
+};
+let hanldeChooseProvice = (item: string) => {
+  console.log(item);
+};
+</script>
 
 <tyle lang="scss" scoped></tyle>
